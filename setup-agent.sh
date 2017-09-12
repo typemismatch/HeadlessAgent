54e644316503fc9622cb50efa426761d31e091d7
typemismatch <craig@ip80.com>
2017-09-12 14:47:05 -0400

    added display service

--- setup-agent.sh
+++ setup-agent.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 npm install
+
 systemctl enable /home/agent/HeadlessAgent/ratchet-agent.service
 systemctl start ratchet-agent.service
+
+systemctl enable /home/agent/HeadlessAgent/ratchet-display.service
+systemctl start ratchet-display.service
