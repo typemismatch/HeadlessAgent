bffba56f055d18c666869051b36fd56ecc4ee0d1
typemismatch <craig@ip80.com>
2017-09-12 20:24:02 -0400

    parse correctly

--- ratchet-agent.js
+++ ratchet-agent.js
@@ -4,7 +4,7 @@ var async               = require('async');
 var networkutils        = require("./networkUtils");
 var ourIPAddress        = networkutils.getFirstAvailableNetworkAddress("eth0,wlan0");
 //var ourMACAddress       = networkutils.getFirstAvailableMACAddress("eth0,wlan0");
-var ourMACAddress       = fs.readFileSync("/sys/class/net/eth0/address"); // os.networkInterfaces fails on this node
+var ourMACAddress       = fs.readFileSync("/sys/class/net/eth0/address").toString().replace('\n',''); // os.networkInterfaces fails on this node
 var deviceConfig        = JSON.parse(fs.readFileSync("device.config.json", 'utf8'));
 var workingPath         = "/home/agent/HeadlessAgent/";
 
